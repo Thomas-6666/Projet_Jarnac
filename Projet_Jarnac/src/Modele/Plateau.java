03ff8355b7125d038459d4755f464ada01f3d1c7
zpilia <zpilia2@gmail.com>
2022-01-10 08:41:53 +0100

    class BaseDeDonnee + Plateau

--- Projet_Jarnac/src/Modele/Plateau.java
+++ Projet_Jarnac/src/Modele/Plateau.java
@@ -1,4 +1,18 @@
 package Modele;
 
+import java.util.List;
+
 public class Plateau {
+
+    public List<BaseDeDonnee> mot;
+
+    public Plateau(List<BaseDeDonnee> M){
+        this.mot = M;
+    }
+
+    public void ajouterMot(){
+        
+    }
+
+
 }
