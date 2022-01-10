1cd5dbaec187ec24d85b68422ee8352bd8fb575f
KevTC01 <allartkevin4@gmail.com>
2022-01-10 08:50:18 +0100

    Creer joueur
    Creation classe joueur

--- Projet_Jarnac/src/Modele/Joueur.java
+++ Projet_Jarnac/src/Modele/Joueur.java
@@ -1,4 +1,27 @@
 package Modele;
 
-public class Joueur {
+public class Joueur{
+    private String nom;
+    private int score = 0;
+    private Reserve reserve;
+    private Partie partie;
+    private Plateau plateau;
+
+    public Joueur(String n, Partie p){
+        nom = n;
+        partie = p;
+        reserve = new Reserve(this);
+        plateau = new Plateau();
+    }
+
+    public void setScore(int s){
+        score = s;
+    }
+
+    public void echangerLettre(String l){
+        reserve.remove(l);
+        partie.getReserve().ajouterLettre(l);
+        reserve.ajouterLettre(partie.getReserve().piocher());
+    }
+
 }
