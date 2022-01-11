34929c9b331d75f3c00cd2b9417a19e35243f1ec
LBasil <79138368+LBasil@users.noreply.github.com>
2022-01-11 10:12:49 +0100

    BBD + des trucs random ?

--- Projet_Jarnac/src/Main.java
+++ Projet_Jarnac/src/Main.java
@@ -3,6 +3,7 @@ import Modele.BaseDeDonnee;
 public class Main {
     public static void main(String args[]){
         BaseDeDonnee bdd = new BaseDeDonnee();
-        bdd.liretoutlefichier();
+        //bdd.liretoutlefichier();
+        bdd.verification("zythums");
     }
 }
--- Projet_Jarnac/src/Modele/BaseDeDonnee.java
+++ Projet_Jarnac/src/Modele/BaseDeDonnee.java
@@ -30,10 +30,39 @@ public class BaseDeDonnee {
             e.printStackTrace();
         }
     }
+
+    public boolean verification(String mot){
+        try
+        {
+            // Le fichier d'entrée
+            File file = new File("BDD.txt");
+            // Créer l'objet File Reader
+            FileReader fr = new FileReader(file);
+            // Créer l'objet BufferedReader
+            BufferedReader br = new BufferedReader(fr);
+            StringBuffer sb = new StringBuffer();
+            String line;
+            while((line = br.readLine()) != null) {
+                mot = mot.toUpperCase();
+                System.out.println(mot);
+                System.out.println(line);
+                if (line == mot) {
+                    fr.close();
+                    System.out.println("C'est true");
+                    return true;
+                }
+            }
+            fr.close();
+            System.out.println("C'est false");
+            return false;
+        }
+        catch(IOException e)
+        {
+            e.printStackTrace();
+            //Pas sûr de ce return false, mais si je le met pas, ça marche pas.
+            System.out.println("C'est false + exception");
+            return false;
+        }
+    }
 }
 
-
-
-
-
-}
