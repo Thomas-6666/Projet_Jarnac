c3a04c29dd1dc5b80d4f78a8c24b4fdf06297ec6
Thomas-6666 <72489077+Thomas-6666@users.noreply.github.com>
2022-01-21 13:21:47 +0100

    L'INTERFACE PEUT ENFIN S'AFFICHER

--- Projet_Jarnac/src/Controlleur/Controller.java
+++ Projet_Jarnac/src/Controlleur/Controller.java
@@ -7,7 +7,6 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
-import javafx.stage.Stage;
 
 public class Controller {
 
@@ -67,25 +66,22 @@ public class Controller {
 
     @FXML
     public void initialize() {
-        assert abandonner != null : "fx:id=\"abandonner\" was not injected: check your FXML file 'Maquettage.fxml'.";
-        assert aide != null : "fx:id=\"aide\" was not injected: check your FXML file 'Maquettage.fxml'.";
-        assert echanger != null : "fx:id=\"echanger\" was not injected: check your FXML file 'Maquettage.fxml'.";
-        assert input_j1 != null : "fx:id=\"input_j1\" was not injected: check your FXML file 'Maquettage.fxml'.";
-        assert input_j2 != null : "fx:id=\"input_j2\" was not injected: check your FXML file 'Maquettage.fxml'.";
-        assert jarnac != null : "fx:id=\"jarnac\" was not injected: check your FXML file 'Maquettage.fxml'.";
-        assert joueur1 != null : "fx:id=\"joueur1\" was not injected: check your FXML file 'Maquettage.fxml'.";
-        assert joueur2 != null : "fx:id=\"joueur2\" was not injected: check your FXML file 'Maquettage.fxml'.";
-        assert lettres_j1 != null : "fx:id=\"lettres_j1\" was not injected: check your FXML file 'Maquettage.fxml'.";
-        assert lettres_j2 != null : "fx:id=\"lettres_j2\" was not injected: check your FXML file 'Maquettage.fxml'.";
-        assert passer != null : "fx:id=\"passer\" was not injected: check your FXML file 'Maquettage.fxml'.";
-        assert piocher != null : "fx:id=\"piocher\" was not injected: check your FXML file 'Maquettage.fxml'.";
-        assert plateau_j1 != null : "fx:id=\"plateau_j1\" was not injected: check your FXML file 'Maquettage.fxml'.";
-        assert plateau_j2 != null : "fx:id=\"plateau_j2\" was not injected: check your FXML file 'Maquettage.fxml'.";
-        assert points_j1 != null : "fx:id=\"points_j1\" was not injected: check your FXML file 'Maquettage.fxml'.";
-        assert points_j2 != null : "fx:id=\"points_j2\" was not injected: check your FXML file 'Maquettage.fxml'.";
-    }
-
-    public void start(Stage stage){
+        assert abandonner != null : "fx:id=\"abandonner\" was not injected: check your FXML file 'Interface.fxml'.";
+        assert aide != null : "fx:id=\"aide\" was not injected: check your FXML file 'Interface.fxml'.";
+        assert echanger != null : "fx:id=\"echanger\" was not injected: check your FXML file 'Interface.fxml'.";
+        assert input_j1 != null : "fx:id=\"input_j1\" was not injected: check your FXML file 'Interface.fxml'.";
+        assert input_j2 != null : "fx:id=\"input_j2\" was not injected: check your FXML file 'Interface.fxml'.";
+        assert jarnac != null : "fx:id=\"jarnac\" was not injected: check your FXML file 'Interface.fxml'.";
+        assert joueur1 != null : "fx:id=\"joueur1\" was not injected: check your FXML file 'Interface.fxml'.";
+        assert joueur2 != null : "fx:id=\"joueur2\" was not injected: check your FXML file 'Interface.fxml'.";
+        assert lettres_j1 != null : "fx:id=\"lettres_j1\" was not injected: check your FXML file 'Interface.fxml'.";
+        assert lettres_j2 != null : "fx:id=\"lettres_j2\" was not injected: check your FXML file 'Interface.fxml'.";
+        assert passer != null : "fx:id=\"passer\" was not injected: check your FXML file 'Interface.fxml'.";
+        assert piocher != null : "fx:id=\"piocher\" was not injected: check your FXML file 'Interface.fxml'.";
+        assert plateau_j1 != null : "fx:id=\"plateau_j1\" was not injected: check your FXML file 'Interface.fxml'.";
+        assert plateau_j2 != null : "fx:id=\"plateau_j2\" was not injected: check your FXML file 'Interface.fxml'.";
+        assert points_j1 != null : "fx:id=\"points_j1\" was not injected: check your FXML file 'Interface.fxml'.";
+        assert points_j2 != null : "fx:id=\"points_j2\" was not injected: check your FXML file 'Interface.fxml'.";
 
     }
 
--- Projet_Jarnac/src/Main.java
+++ Projet_Jarnac/src/Main.java
@@ -1,23 +1,32 @@
+import Controlleur.Controller;
 import Modele.Partie;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
+import java.io.IOException;
 
 public class Main extends Application {
 
     @Override
-    public void start(Stage primaryStage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("Interface.fxml"));
-
-        primaryStage.setScene(new Scene(root));
-        primaryStage.show();
+    public void start(Stage primaryStage) throws IOException {
+        try {
+            Controlleur.Controller ctrl = new Controller();
+            Scene scene = new FXMLLoader(getClass().getResource("Interface.fxml")).load();
+            ctrl.initialize();
+            primaryStage.setTitle("Jarnac");
+            primaryStage.setScene(scene);
+            primaryStage.show();
+        } catch (IOException exception) {
+            throw new RuntimeException(exception);
+        }
     }
 
     public static void main(String[] args) {
         //Ca va faire une erreur si vous retirez les commentaires ici, faut que je fixe ça la prochaine fois
-        //launch(args);
+        launch(args);
 
         Partie p = new Partie();
     }
