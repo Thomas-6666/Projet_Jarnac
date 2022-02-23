3644177793be2ec291665a1f5a15c4eb96d601ca
LBasil <79138368+LBasil@users.noreply.github.com>
2022-02-23 11:12:08 +0100

    DE LA MUSIQUE
    MUSIQUE !!!!!

--- Projet_Jarnac/src/Main.java
+++ Projet_Jarnac/src/Main.java
@@ -1,11 +1,18 @@
 import Controlleur.Controller;
 import Modele.Partie;
 import javafx.application.Application;
+import javafx.embed.swing.JFXPanel;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.*;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
 import javafx.stage.*;
 
+import javax.swing.*;
+import java.io.File;
 import java.io.IOException;
+import java.nio.file.Paths;
+
 
 public class Main extends Application {
 
@@ -22,6 +29,7 @@ public class Main extends Application {
             Controller.getInstance().initplateau();
 
             new Vue.Popup(primaryStage, 1);
+            startMusic();
 
         } catch (IOException exception) {
             throw new RuntimeException(exception);
@@ -33,4 +41,16 @@ public class Main extends Application {
         ctrl.initialize();
         launch(args);
     }
+
+    MediaPlayer mediaPlayer;
+    public void startMusic(){
+        String s = "src/ressources/music.mp3";
+        Media m = new Media(Paths.get(s).toUri().toString());
+        mediaPlayer = new MediaPlayer(m);
+        mediaPlayer.play();
+    }
+
+
+
+
 }
